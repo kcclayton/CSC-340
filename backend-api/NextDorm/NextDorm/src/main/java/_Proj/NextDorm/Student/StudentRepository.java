0e19d42de811db664cacdc5ccdfc94d8d7743f2b
Katie <kalufukatie@gmail.com>
2026-03-19 11:02:05 -0400

    Added lombok dependency, user.java functionality, and Student -> including the controller, repository, service, and .java

--- backend-api/NextDorm/NextDorm/src/main/java/_Proj/NextDorm/Student/StudentRepository.java
+++ backend-api/NextDorm/NextDorm/src/main/java/_Proj/NextDorm/Student/StudentRepository.java
@@ -1,5 +1,10 @@
 package _Proj.NextDorm.Student;
 
-public class StudentRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByEmail(String email);
+    List<Student> findByResidenceHall(String residenceHall);
 }
--- backend-api/NextDorm/NextDorm/src/main/java/_Proj/NextDorm/User/User.java
+++ backend-api/NextDorm/NextDorm/src/main/java/_Proj/NextDorm/User/User.java
@@ -1,5 +1,42 @@
 package _Proj.NextDorm.User;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public enum UserRole {
+        STUDENT,
+        RA
+    }
+
+
 }
