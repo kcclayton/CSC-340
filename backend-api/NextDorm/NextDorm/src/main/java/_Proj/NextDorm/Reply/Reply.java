ef43ecfa1bbef030f11643c44fa1b346feaeaf0c
Katie <kalufukatie@gmail.com>
2026-03-23 17:08:05 -0400

    Added in functionality for reply, including reply.java, replycontroller, replyrepository, and replyservice. Edited post.java and student.java to include mappings to reply.

--- backend-api/NextDorm/NextDorm/src/main/java/_Proj/NextDorm/Reply/Reply.java
+++ backend-api/NextDorm/NextDorm/src/main/java/_Proj/NextDorm/Reply/Reply.java
@@ -1,5 +1,45 @@
 package _Proj.NextDorm.Reply;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import _Proj.NextDorm.Post.Post;
+import _Proj.NextDorm.Student.Student;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "replies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reply {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyID;
+
+    @Column(nullable = false)
+    private String replyContent;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    @JsonIgnoreProperties({"replies", "student"})
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+@   JsonIgnoreProperties({"posts", "replies"}) 
+    private Student student;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
